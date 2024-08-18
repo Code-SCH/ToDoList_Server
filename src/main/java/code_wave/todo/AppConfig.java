92f6fbbe6b2dce968ebecf9d51dd4555598413ee
yih5025 <yih5025@naver.com>
2024-08-18 19:04:44 +0900

    api test

--- src/main/java/code_wave/todo/AppConfig.java
+++ src/main/java/code_wave/todo/AppConfig.java
@@ -1,9 +1,9 @@
 package code_wave.todo;
 
 import code_wave.todo.service.ShortTermWeatherAPI;
-import code_wave.todo.service.TodoService;
+//import code_wave.todo.service.TodoService;
 import code_wave.todo.service.WeatherAPI;
-import code_wave.todo.repository.TodoRepository;
+//import code_wave.todo.repository.TodoRepository;
 import code_wave.todo.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,10 +17,10 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
 
     // Bean for the TodoService
-    @Bean
-    public TodoService todoService(TodoRepository todoRepository, UserRepository userRepository) {
-        return new TodoService(todoRepository, userRepository);
-    }
+//    @Bean
+//    public TodoService todoService(TodoRepository todoRepository, UserRepository userRepository) {
+//        return new TodoService(todoRepository, userRepository);
+//    }
 
     // Bean for the WeatherAPI
     @Bean
--- src/main/java/code_wave/todo/service/TodoService.java
+++ src/main/java/code_wave/todo/service/TodoService.java
@@ -1,28 +1,8 @@
-package code_wave.todo.service;
-
-import code_wave.todo.domain.Todo;
-import code_wave.todo.repository.TodoRepository;
-import code_wave.todo.repository.UserRepository;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-@Getter
-@Setter
-public class TodoService {
-
-    private final TodoRepository todoRepository;
-    Todo todo = new Todo();
-
-    @Autowired
-    public TodoService(TodoRepository todoRepository, UserRepository userRepository){this.todoRepository = todoRepository;}
-}
 //package code_wave.todo.service;
 //
 //import code_wave.todo.domain.Todo;
 //import code_wave.todo.repository.TodoRepository;
+//import code_wave.todo.repository.UserRepository;
 //import lombok.Getter;
 //import lombok.Setter;
 //import org.springframework.beans.factory.annotation.Autowired;
@@ -37,10 +17,5 @@ public class TodoService {
 //    Todo todo = new Todo();
 //
 //    @Autowired
-//    public TodoService(TodoRepository todoRepository){this.todoRepository = todoRepository;}
-//
-//
-//
-//
-//
+//    public TodoService(TodoRepository todoRepository, UserRepository userRepository){this.todoRepository = todoRepository;}
 //}
