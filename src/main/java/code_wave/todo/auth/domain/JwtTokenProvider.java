09bf2d5fac293e57ca7ddb870ff85ec01a371adf
giyomdabin <dabinjang01@gmail.com>
2024-08-18 16:57:22 +0900

    jwt token2

---
+++ src/main/java/code_wave/todo/auth/domain/JwtTokenProvider.java
@@ -0,0 +1,11 @@
+package code_wave.todo.auth.domain;
+
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+
+@Component
+public class JwtTokenProvider {
+
+}
