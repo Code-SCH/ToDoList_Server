729174ab48f96ee916583637c428baf6f3959f4b
Shs0160 <aladinsue@gmail.com>
2024-08-18 12:38:03 +0900

    hi

---
+++ src/test/java/code_wave/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package code_wave.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
